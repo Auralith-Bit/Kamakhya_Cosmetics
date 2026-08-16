f4dafab5e6ed6a6e9bf022387078bbae3daa1faa
bibisha2 <138382324+Bibisha2@users.noreply.github.com>
2026-08-16 16:14:07 +0545

    Restore homepage sections

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,13 +6,17 @@ import React from "react";
 import HeroSection from "../components/HeroSection/HeroSection";
 import AboutStory from "../components/Aboutus/AboutStory";
 import FeaturedCollection from "../components/FeaturedCollection/FeaturedCollection";
+import StatsBar from "../components/StatsBar/StatsBar";
+import BrandsSection from "../components/BrandsSection/BrandsSection";
 
 const HomePage = () => (
   <>
     <div id="home"><HeroSection /></div>
-    <div id="products"><FeaturedCollection /></div>
-    <div id="manufacturing"></div>
     <div id="about"><AboutStory /></div>
+    <div id="products"><FeaturedCollection /></div>
+    <StatsBar />
+    <BrandsSection />
+    <div id="manufacturing"></div>
     <div id="contact"></div>
   </>
 );
